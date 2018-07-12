829aa5afbf3a78d766115b8af9b2c1c2e0ca4a3e
eedamame <eedamame@gmail.com>
2018-07-12 17:34:46 +0900

    9.3.1の演習の前まで

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,10 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ログイン後
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
-
     else
       flash.now[:danger] = "emailまたはパスワードが間違っています"
       render 'new'
@@ -16,7 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 
